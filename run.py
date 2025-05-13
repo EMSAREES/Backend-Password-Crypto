696062a0d99cce333677a3d4be4add31810a9860
Emilio S. Reyes E. <Lobomategamer@gmail.com>
2025-05-13 02:05:40 -0700

    corregir algunas cosas

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()  # Crea la instancia de la aplicación
 
 if __name__ == "__main__":
     # Ejecuta la app en modo desarrollo (con recarga automática y errores detallados)
-    app.run(debug=True, host="0.0.0", port=os.getenv("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", 5000))
